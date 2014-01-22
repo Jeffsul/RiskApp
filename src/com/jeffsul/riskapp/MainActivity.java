b195b81c4ba7ed92622249fc00d5227f5f098809
Jeffsul <jeffsul16@gmail.com>
2014-01-22 09:11:12 -0500

    Player Number Spinner in Main Activity

--- src/com/jeffsul/riskapp/MainActivity.java
+++ src/com/jeffsul/riskapp/MainActivity.java
@@ -3,13 +3,27 @@ package com.jeffsul.riskapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity {	
+	private static final int MAX_NUM_PLAYERS = 6;
+	private static final int MIN_NUM_PLAYERS = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.number_players_spinner);
+		CharSequence[] objects = new CharSequence[MAX_NUM_PLAYERS - MIN_NUM_PLAYERS + 1];
+		for (int i = 0; i < objects.length; i++) {
+			objects[i] = Integer.toString(i + MIN_NUM_PLAYERS);
+		}
+		ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+				android.R.layout.simple_spinner_item, objects);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 
 	@Override
