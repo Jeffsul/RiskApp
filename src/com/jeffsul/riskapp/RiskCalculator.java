2ae3dd33b41a5024eacee665dd40a33aac11a086
Jeffsul <jeffsul16@gmail.com>
2014-02-01 15:02:30 -0500

    Cleaning up RiskCalculator

--- src/com/jeffsul/riskapp/RiskCalculator.java
+++ src/com/jeffsul/riskapp/RiskCalculator.java
@@ -18,24 +18,20 @@ public class RiskCalculator {
 	public static final double L3V2 = 2275.0 / 7776.0;
 	public static final double T3V2 = 2611.0 / 7776.0;
 	
-	private static NumberFormat format = NumberFormat.getPercentInstance(); {
-		format.setMaximumFractionDigits(6);
-		format.setMinimumFractionDigits(6);
-	}
+	private static NumberFormat format = NumberFormat.getPercentInstance();
 	
 	public static String getResults(int attackers, int defenders) {
 		double[][] outcome = new double[attackers + 1][defenders + 1];
 		for (int i = 0; i < attackers + 1; i++) {
 			for (int j = 0; j < defenders + 1; j++) {
-				outcome[i][j] = -1.0;
+				outcome[i][j] = -1;
 			}
 		}
-		outcome[attackers][defenders] = 1.0;
+		outcome[attackers][defenders] = 1;
 		
-		double winOdds = 0.0;
+		double winOdds = 0;
 		String likelyOutcomeState = null;
-		double likelyOutcome = -1.0;
-		
+		double likelyOutcome = -1;
 		for (int i = attackers; i >= 2; i--) {
 			winOdds += getOdds(i, 0, outcome, attackers, defenders);
 			if (outcome[i][0] > likelyOutcome) {
@@ -54,77 +50,71 @@ public class RiskCalculator {
 	
 	public static double getWinningOdds(int attackers, int defenders) {
 		double[][] outcome = new double[attackers + 1][defenders + 1];
-		for (int i = 0; i < attackers + 1; i++)
-			for (int j = 0; j < defenders + 1; j++)
-				outcome[i][j] = -1.0;
-		outcome[attackers][defenders] = 1.0;
-		
-		double winOdds = 0.0;
-		double likelyOutcome = -1.0;
-		
-		for (int i = attackers; i >= 2; i--)
-		{
-			winOdds += getOdds(i, 0, outcome, attackers, defenders);
-			if (outcome[i][0] > likelyOutcome)
-				likelyOutcome = outcome[i][0];
+		for (int i = 0; i < attackers + 1; i++) {
+			for (int j = 0; j < defenders + 1; j++) {
+				outcome[i][j] = -1;
+			}
 		}
-		for (int i = 1; i <= defenders; i++)
-		{
-			if (outcome[1][i] > likelyOutcome)
-				likelyOutcome = outcome[1][i];
+		outcome[attackers][defenders] = 1;
+		
+		double winOdds = 0;		
+		for (int i = attackers; i >= 2; i--) {
+			winOdds += getOdds(i, 0, outcome, attackers, defenders);
 		}
 		return winOdds;
 	}
 	
 	private static double getOdds(int a, int d, double[][] outcome, int attackers, int defenders) {
-		if (outcome[a][d] != -1.0)
+		if (outcome[a][d] != -1) {
 			return outcome[a][d];
+		}
 		
-		double odds = 0.0;
+		double odds = 0;
 		if (a + 2 <= attackers && d >= 2) {
-			if (a + 2 >= 4)
+			if (a + 2 >= 4) {
 				odds += L3V2 * getOdds(a + 2, d, outcome, attackers, defenders);
-			else if (a + 2 == 3)
+			} else if (a + 2 == 3) {
 				odds += L2V2 * getOdds(a + 2, d, outcome, attackers, defenders);
+			}
 		}
 		if (d + 2 <= defenders && a >= 3) {
-			if (a >= 4)
+			if (a >= 4) {
 				odds += W3V2 * getOdds(a, d + 2, outcome, attackers, defenders);
-			else if (a == 3)
+			} else if (a == 3) {
 				odds += W2V2 * getOdds(a, d + 2, outcome, attackers, defenders);
+			}
 		}
 		if (a + 1 <= attackers && d + 1 <= defenders && d + 1 >= 2 && a + 1 >= 3) {
-			if (a + 1 >= 4)
+			if (a + 1 >= 4) {
 				odds += T3V2 * getOdds(a + 1, d + 1, outcome, attackers, defenders);
-			else if (a + 1 == 3)
+			} else if (a + 1 == 3) {
 				odds += T2V2 * getOdds(a + 1, d + 1, outcome, attackers, defenders);
+			}
 		}
 		if (a + 1 <= attackers) {
-			if (a + 1 == 2 && d >= 2)
+			if (a + 1 == 2 && d >= 2) {
 				odds += L1V2 * getOdds(a + 1, d, outcome, attackers, defenders);
-			else if (a + 1 >= 4 && d == 1)
+			} else if (a + 1 >= 4 && d == 1) {
 				odds += L3V1 * getOdds(a + 1, d, outcome, attackers, defenders);
-			else if (a + 1 == 3 && d == 1)
+			} else if (a + 1 == 3 && d == 1) {
 				odds += L2V1 * getOdds(a + 1, d, outcome, attackers, defenders);
-			else if (a + 1 == 2 && d == 1)
+			} else if (a + 1 == 2 && d == 1) {
 				odds += L1V1 * getOdds(a + 1, d, outcome, attackers, defenders);
+			}
 		}
 		if (d + 1 <= defenders) {
-			if (a == 2 && d + 1 >= 2)
+			if (a == 2 && d + 1 >= 2) {
 				odds += W1V2 * getOdds(a, d + 1, outcome, attackers, defenders);
-			else if (a >= 4 && d + 1 == 1)
+			} else if (a >= 4 && d + 1 == 1) {
 				odds += W3V1 * getOdds(a, d + 1, outcome, attackers, defenders);
-			else if (a == 3 && d + 1 == 1)
+			} else if (a == 3 && d + 1 == 1) {
 				odds += W2V1 * getOdds(a, d + 1, outcome, attackers, defenders);
-			else if (a == 2 && d + 1 == 1)
+			} else if (a == 2 && d + 1 == 1) {
 				odds += W1V1 * getOdds(a, d + 1, outcome, attackers, defenders);
+			}
 		}
 		outcome[a][d] = odds;
 		return odds;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(getResults(10, 3));
 	}
 }
 
