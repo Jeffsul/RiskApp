db80c46748bd3645df4043cae8286077184d9a47
Jeffsul <jeffsul16@gmail.com>
2014-03-31 09:57:53 -0400

    Adding database utilities

---
+++ src/com/jeffsul/riskapp/db/RiskGameContract.java
@@ -0,0 +1,25 @@
+package com.jeffsul.riskapp.db;
+
+import android.provider.BaseColumns;
+
+public final class RiskGameContract {
+
+	// Empty constructor to prevent someone from accidentally instantiating it.
+	public RiskGameContract() {}
+	
+	public static abstract class RiskGame implements BaseColumns {
+		public static final String TABLE_NAME = "games";
+		public static final String COLUMN_NAME_CREATED = "date_created";
+		public static final String COLUMN_NAME_LAST_PLAYED = "last_played";
+		public static final String COLUMN_NAME_MAP_ID = "map_id";
+		public static final String COLUMN_NAME_TURN_COUNTER = "turn_counter";
+	}
+	
+	public static abstract class RiskGamePlayers implements BaseColumns {
+		public static final String TABLE_NAME = "gamePlayers";
+		public static final String COLUMN_NAME_GAME_ID = "game_id";
+		public static final String COLUMN_NAME_PLAYER_NAME = "player_name";
+		public static final String COLUMN_NAME_PLAYER_ID = "player_id";
+		public static final String COLUMN_NAME_PLAYER_POSITION = "player_pos";
+	}
+}
